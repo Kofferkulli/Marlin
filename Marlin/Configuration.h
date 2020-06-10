7d6dff47b48b9aa1dfdb00d0ae6f33d9d6c9d93c
David Bosen <david.bosen@rwth-aachen.de>
2020-06-10 17:43:06 +0200

    change MIN_PROBE_EDGE to 3mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1020,7 +1020,11 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
+<<<<<<< HEAD
 #define PROBING_MARGIN 10
+=======
+#define MIN_PROBE_EDGE 3
+>>>>>>> change MIN_PROBE_EDGE to 3mm
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
