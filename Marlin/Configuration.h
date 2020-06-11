ccb647268ca30d5bf6aa0bf4c240efa678046453
David Bosen <david.bosen@rwth-aachen.de>
2020-06-11 14:36:43 +0200

    change E steps to 101

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -767,7 +767,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 104.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 101 }
 
 /**
  * Default Max Feed Rate (mm/s)
