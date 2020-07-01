92c3450c3bc5f6f8d5aeafe395325b2e038c57f5
David Bosen <david.bosen@rwth-aachen.de>
2020-07-01 16:45:51 +0200

    change to glass bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1165,10 +1165,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0 //-15 for glass
+#define Y_MIN_POS -15 //-15 for glass
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + 13)
-#define Y_MAX_POS Y_BED_SIZE //220 for glass
+#define Y_MAX_POS 220 //220 for glass, Y_BED_SIZE else
 #define Z_MAX_POS 220
 
 /**
